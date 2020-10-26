f03fef11746fa3500924eae77af63173cd448404
Amedeo Salvati <amedeo@linux.com>
2020-10-26 17:06:04 +0100

    add vm-hardening.yaml in order to apply hardening before the make template

--- create-ovirt-template.sh
+++ create-ovirt-template.sh
@@ -2,11 +2,16 @@
 #
 ANSIBLE_PLAYBOOK=$(which ansible-playbook)
 VAULT_FILE=vault-file
-PLAYBOOKS=(clean.yaml satellite.yaml ovirt.yaml vm-customize.yaml vm-cleanup.yml ovirt-make-template.yaml clean.yaml)
+PLAYBOOKS=(clean.yaml satellite.yaml ovirt.yaml vm-customize.yaml vm-hardening.yaml vm-cleanup.yml ovirt-make-template.yaml clean.yaml)
 
 for playbook in "${PLAYBOOKS[@]}"; do
     echo "Run playbook: ${playbook}"
-    $ANSIBLE_PLAYBOOK --vault-password-file $VAULT_FILE ${playbook}
+    if [ "${playbook}" == "vm-hardening.yaml" ]; then
+        echo "run with hosts file"
+        $ANSIBLE_PLAYBOOK --vault-password-file $VAULT_FILE -i hosts-templaterhv ${playbook}
+    else
+        $ANSIBLE_PLAYBOOK --vault-password-file $VAULT_FILE ${playbook}
+    fi
     STATUS=$?
     if [ ${STATUS} -gt 0 ]; then
         echo "Error in playbook ${playbook}... Exit"
